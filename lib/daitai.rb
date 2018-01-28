b81eb121b1919797cc86501f31ba9e31adfaaf5b
Walerian <walerian.sobczak@getbase.com>
2018-01-28 19:32:19 +0100

    Added Proc and Hash handling for the `map` function.

--- lib/daitai.rb
+++ lib/daitai.rb
@@ -2,4 +2,5 @@ require 'daitai/map'
 require 'daitai/version'
 
 module Daitai
+  extend Map
 end
--- lib/daitai/map.rb
+++ lib/daitai/map.rb
@@ -1,6 +1,27 @@
 module Daitai
-  def map
-    ->(fn, functor) { functor.map(&fn) }.curry
+  module Map
+    def map
+      lambda do |fn, functor|
+        case functor
+        when Proc then proc_map(fn, functor)
+        when Hash then hash_map(fn, functor)
+        else default_map(fn, functor)
+        end
+      end.curry
+    end
+
+    private
+
+    def proc_map(fn, functor)
+      ->(*args) { fn.(*functor.(*args)) }
+    end
+
+    def hash_map(fn, functor)
+      Hash[functor.map { |key, value| [key, fn.(value)] }]
+    end
+
+    def default_map(fn, functor)
+      functor.map(&fn)
+    end
   end
-  module_function :map
 end
