f585ee84064b96c579ed08983b46ebac674904c9
Walerian <walerian.sobczak@getbase.com>
2018-02-03 15:12:52 +0100

    Added specs for the `filter` function.

---
+++ spec/daitai/filter_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe Daitai::Filter do
+  before do
+    @even = ->(x) { x % 2 == 0 } # rubocop:disable Style/EvenOdd
+  end
+
+  it 'filters an array' do
+    expect(Daitai.filter.(@even, [1, 2, 3, 4])).to eql([2, 4])
+  end
+
+  it 'returns an empty array for no matching elements' do
+    expect(Daitai.filter.(->(x) { x > 10 }, [1, 2, 9, 10])).to eql([])
+  end
+
+  it 'returns an empty array for an empty filterable' do
+    expect(Daitai.filter.(@even, [])).to eql([])
+  end
+
+  it 'filters a hash' do
+    expect(Daitai.filter.(@even, w: 1, x: 2, y: 3, z: 4)).to eql(x: 2, z: 4)
+  end
+
+  it 'is curried' do
+    take_even = Daitai.filter.(@even)
+    expect(take_even.([1, 2, 3, 4])).to eql([2, 4])
+  end
+end
