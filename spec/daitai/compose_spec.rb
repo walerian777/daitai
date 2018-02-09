d5013d27cd84f98888472d6146d7c2e52558968e
Walerian <walerian.sobczak@getbase.com>
2018-02-09 17:15:09 +0100

    Added specs for the `compose` function.

---
+++ spec/daitai/compose_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe Daitai::Compose do
+  before do
+    @to_int   = ->(x) { x.to_i }
+    @plus_two = ->(x) { x + 2 }
+    @square   = ->(x) { x * x }
+  end
+
+  it 'composes functions from right to left' do
+    f = Daitai.compose.(@square, @plus_two, @to_int)
+    expect(f.('10')).to eql(144)
+  end
+end
