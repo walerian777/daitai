34304f9a6edd0f8d7b2fbedca4405429d3c50f90
Walerian <walerian.sobczak@getbase.com>
2018-02-11 12:11:27 +0100

    Added specs for the `pipe` function.

---
+++ spec/daitai/pipe_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe Daitai::Pipe do
+  before do
+    @to_int   = ->(x) { x.to_i }
+    @plus_two = ->(x) { x + 2 }
+    @square   = ->(x) { x * x }
+  end
+
+  it 'composes functions from left to right' do
+    f = Daitai.pipe.(@to_int, @plus_two, @square)
+    expect(f.('10')).to eql(144)
+  end
+end
