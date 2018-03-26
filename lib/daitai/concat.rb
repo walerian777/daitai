fdd6a8d4612d12b70518667891cbbd865c88b2a7
Walerian <walerian.sobczak@getbase.com>
2018-03-26 18:03:39 +0200

    Fixed immutability issue in the `concat` function.

--- lib/daitai/concat.rb
+++ lib/daitai/concat.rb
@@ -3,22 +3,7 @@
 module Daitai
   module Concat
     def concat
-      lambda do |x, y|
-        case x
-        when String then string_concat(x, y)
-        else default_concat(x, y)
-        end
-      end.curry
-    end
-
-    private
-
-    def string_concat(x, y)
-      x + y
-    end
-
-    def default_concat(x, y)
-      x.concat(y)
+      ->(x, y) { x + y }.curry
     end
   end
 end
--- spec/daitai/concat_spec.rb
+++ spec/daitai/concat_spec.rb
@@ -1,20 +1,58 @@
 # frozen_string_literal: true
 
 RSpec.describe Daitai::Concat do
-  it 'concatenates two arrays' do
-    expect(Daitai.concat.([1, 2], [3, 4])).to eql([1, 2, 3, 4])
+  context 'for String arguments' do
+    before do
+      @beginning = 'Szcz'
+      @ending = 'ecin'
+    end
+
+    it 'concatenates two strings' do
+      expect(Daitai.concat.(@beginning, @ending)).to eql('Szczecin')
+    end
+
+    it 'returns an empty string for empty strings' do
+      expect(Daitai.concat.('', '')).to eql('')
+    end
+
+    it 'does not mutate the first argument' do
+      expect do
+        Daitai.concat.(@beginning, @ending)
+      end.not_to(change { @beginning })
+    end
+
+    it 'does not mutate the second argument' do
+      expect do
+        Daitai.concat.(@beginning, @ending)
+      end.not_to(change { @ending })
+    end
   end
 
-  it 'concatenates two strings' do
-    expect(Daitai.concat.('Szcz', 'ecin')).to eql('Szczecin')
-  end
+  context 'for Array arguments' do
+    before do
+      @beginning = [1, 2]
+      @ending = [3, 4]
+    end
 
-  it 'returns an empty list for empty lists' do
-    expect(Daitai.concat.([], [])).to eql([])
-  end
+    it 'concatenates two arrays' do
+      expect(Daitai.concat.(@beginning, @ending)).to eql([1, 2, 3, 4])
+    end
 
-  it 'returns an empty string for empty strings' do
-    expect(Daitai.concat.('', '')).to eql('')
+    it 'returns an empty array for empty arrays' do
+      expect(Daitai.concat.([], [])).to eql([])
+    end
+
+    it 'does not mutate the first argument' do
+      expect do
+        Daitai.concat.(@beginning, @ending)
+      end.not_to(change { @beginning })
+    end
+
+    it 'does not mutate the first argument' do
+      expect do
+        Daitai.concat.(@beginning, @ending)
+      end.not_to(change { @ending })
+    end
   end
 
   it 'is curried' do
